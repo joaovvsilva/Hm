497ceffa00116df3feb1e4e574b59c4490d0218d
joaovvsilva <163225028+joaovvsilva@users.noreply.github.com>
2024-10-03 17:37:15 -0300

    ALTERAÇÕES
    Signed-off-by: joaovvsilva <163225028+joaovvsilva@users.noreply.github.com>

--- app/app.py
+++ app/app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+@app.route('/trabalhos')
+def trabalhos():
+    return render_template('trabalhos.html')
+
+@app.route('/serviços')
+def servicos():
+    return render_template('serviços.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
